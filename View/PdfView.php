9851c8592c1e595d170b95a5a00468da77c7b1b6
Milan van As <milan@hetwonen.nl>
2018-03-23 14:02:32 +0100

    Initial setup

---
+++ View/PdfView.php
@@ -0,0 +1,52 @@
+<?php
+
+App::uses('View', 'View');
+
+use h4cc\WKHTMLToPDF\WKHTMLToPDF;
+use mikehaertl\wkhtmlto\Pdf;
+
+/**
+ * Class PdfView
+ */
+class PdfView extends View {
+
+    public $subDir = 'pdf';
+
+    public function render($view = null, $layout = null) {
+
+        # Render the view
+        $pdfContents = parent::render($view, $layout);
+
+        # Initialize the pdf with the rendered view
+        $pdf = new Pdf($pdfContents);
+
+        # Set the binary
+        $pdf->binary = WKHTMLToPDF::PATH;
+
+        # Set WkToHtmlPdf_options to pdf if we have them
+        if (isset($this->viewVars['WkToHtmlPdf_options'])) {
+            $pdf->setOptions($this->viewVars['WkToHtmlPdf_options']);
+        }
+
+        if (isset($this->viewVars['_save'])) {
+            $pdf->saveAs($this->viewVars['_save']);
+        } else {
+
+            # Type
+            $this->response->type('pdf');
+
+            # Disable cache
+            $this->response->disableCache();
+
+            if (isset($this->viewVars['_download'])) {
+                $pdf->send($this->viewVars['_download'] . '.pdf');
+            }
+
+            if (isset($this->viewVars['_stream'])) {
+                $pdf->send();
+            }
+        }
+
+        return true;
+    }
+}
